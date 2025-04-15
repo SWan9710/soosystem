fd33cdffa7dfd7a16b63c8b40389b605ad6cb010
정수완 <wjdtndhks1212@gmail.com>
2025-04-15 19:19:59 +0900

    feat: 페이지 마운트시 최상단으로 이동하게 수

--- src/pages/Education/EducationIntroduction.jsx
+++ src/pages/Education/EducationIntroduction.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import useScrollTop from "../../hooks/useScrollTop";
 
 import "../../styles/pages/Education/EducationIntroduction.css";
 
 const EducationIntroduction = () => {
+  useScrollTop(0, 0);
   return (
     <div>
       <Header />
