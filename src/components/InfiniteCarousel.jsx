25e003d648311daf0702230799f9778c1b6632b0
정수완 <wjdtndhks1212@gmail.com>
2025-04-30 21:58:17 +0900

    feat: 교육재료사업 소개페이지 제작, 조직도 부분 이미지 처리

--- src/components/InfiniteCarousel.jsx
+++ src/components/InfiniteCarousel.jsx
@@ -7,9 +7,7 @@ const InfiniteCarousel = ({ carouselList }) => {
   const [currentList, setCurrentList] = useState([]);
   const carouselRef = useRef(null);
 
-  let touchStartX;
-  let touchEndX;
-
+  // 받아온 데이터 정보 currentList에 저장
   useEffect(() => {
     if (carouselList.length !== 0) {
       const startData = carouselList[0];
@@ -19,12 +17,14 @@ const InfiniteCarousel = ({ carouselList }) => {
     }
   }, [carouselList]);
 
+  // useEffect에서 인덱스를 주시해서 인덱스가 변경되면 해당 인덱스로 캐러셀 이동
   useEffect(() => {
     if (carouselRef.current) {
       carouselRef.current.style.transform = `translateX(-${currentIndex}00%)`;
     }
   }, [currentIndex]);
 
+  //  슬라이드 점프효과 -> style 일시제거
   const moveToNthSlide = (index) => {
     setTimeout(() => {
       setCurrentIndex(index);
@@ -34,15 +34,19 @@ const InfiniteCarousel = ({ carouselList }) => {
     }, 500);
   };
 
+  // 버튼이벤트
   const handleSwipe = (direction) => {
     const newIndex = currentIndex + direction;
 
+    // 현재 인덱스정보를 토대로 슬라이드 점프
+    // 마지막 슬라이드인 경우 1번으로 이동, 0번인 경우 마지막 슬라이드로 이동
     if (newIndex === carouselList.length + 1) {
       moveToNthSlide(1);
     } else if (newIndex === 0) {
       moveToNthSlide(carouselList.length);
     }
 
+    // 현재 인덱스에 받아온 이동정보를 토대로 인덱스 초기화
     setCurrentIndex((prev) => prev + direction);
     if (carouselRef.current) {
       carouselRef.current.style.transition = "all 0.5s ease-in-out";
@@ -59,13 +63,13 @@ const InfiniteCarousel = ({ carouselList }) => {
           <ChevronRight />
         </button>
         <ul className="carousel" ref={carouselRef}>
-          {currentList.map((image, index) => (
+          {currentList.map((info, index) => (
             <li key={`carousel-${index}`} className="carousel-item">
-              <img src={image[0]} alt="carousel" />
-              <h1 className="carousel-title">{image[1]}</h1>
-              {Array.isArray(image[2]) && (
+              <img src={info[0]} alt="carousel" />
+              <h1 className="carousel-title">{info[1]}</h1>
+              {Array.isArray(info[2]) && (
                 <ul className="carousel-description">
-                  {image[2].map((desc, i) => (
+                  {info[2].map((desc, i) => (
                     <li key={`desc-${i}`}>{desc}</li>
                   ))}
                 </ul>
--- src/pages/Education/EducationIntroduction.jsx
+++ src/pages/Education/EducationIntroduction.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import useScrollTop from "../../hooks/useScrollTop";
+import eduImage from "../../assets/edulogo/sub0201_img01.jpg";
+import eduChartImage from "../../assets/edulogo/eduChart.png";
 
 import "../../styles/pages/Education/EducationIntroduction.css";
 
 const EducationIntroduction = () => {
-  useScrollTop(0, 0);
+  // useScrollTop(0, 0);
   return (
     <div>
       <Header />
@@ -17,28 +19,65 @@ const EducationIntroduction = () => {
         </div>
 
         <div className="education-description-wrap">
-          <div className="education-description-title-wrap">
-            <h1 className="edu-title">금속교육 재료사업</h1>
-            <h4 className="edu-subtitle">
-              대한민국 대표 기술교육재료 제조업체, 기능올림픽 공식 후원사
+          <div className="education-description-titles">
+            <h1>금속교육 재료사업</h1>
+            <h4>
+              대한민국 대표 기술교육재료 제조업체,{" "}
+              <span>국제기능올림픽대회 공식후원사</span>
             </h4>
           </div>
-          <p>
-            수시스템은 산업에 종사하는 기능인이 자신의 기술과 기능을 가정과
-            국가에서 인정받고 우대받는 사회문화가 되길 바라며 먼저 노력하는
-            기업입니다.
-          </p>
-          기술기능교육에 꼭 필요한 기술교육재료를 제조하는 대한민국 대표업체로서
-          선생님의 경험과 노력에 귀 기울이며 교육생의 기술발전에 수시스템은
-          책임과 의무를 가지고 착한 가격에 최고의 품질의 기술교육재료를 개발 및
-          제조 공급하겠습니다.
-          <p></p>
-          <p className="education-deesciption">
-            수시스템의 기술교육재료는{" "}
-            <span>전국 고등학교(약 200곳)와 기술학원(약 57곳)에 납품</span>되고
-            있으며, <span>국내 기술교육재료시장 점유율 1위</span>로
-            <span>전국기능경기대회 시험재료로 사용</span>되고 있습니다.
-          </p>
+          <div className="education-description-top">
+            <h1>사업소개</h1>
+            <p>인류와 환경을 생각하는 생활 과학기업 주식회사 수시스템</p>
+          </div>
+
+          <section className="education-description-second">
+            <h1 className="education-subtitle">기술교육 재료사업</h1>
+            <img className="edu-image" src={eduImage} alt="edu-image" />
+            <div className="education-description-box">
+              <p>
+                수시스템은 산업에 종사하는 기능인이 자신의 기술과 기능을 가정과
+                국가에서 인정받고 우대받는 사회문화가 되길 바라며 먼저 노력하는
+                기업이 되겠습니다.
+              </p>
+              <p>
+                기술기능교육에 꼭 필요한 기술교육재료를 제조하는 대한민국
+                대표업체로서 선생님의 경험과 노력에 귀 기울이며 교육생의
+                기술발전에 수시스템은 책임과 의무를 가지고 착한 가격에 최고의
+                품질의 기술교육재료를 개발 및 제조 공급하겠습니다.
+              </p>
+              <ul>
+                <li>밀링, 선반용 규격소재</li>
+                <li>모든 용접시험 연습, 대회용 재료</li>
+                <li>철판, 파이프, 평철, 빔, 앵글, 환봉 등 철강금속재료 판매</li>
+                <li>
+                  CNC, 레이저, MCT, NC선반, 절단 / 절곡, 용접제관, 홀, 탭, 절삭,
+                  면삭 가공
+                </li>
+                <li>2D, 3D, CAD설계, 제작</li>
+                <li>다양한 상품디자인 개발</li>
+              </ul>
+            </div>
+          </section>
+
+          <section className="education-description-third">
+            <h1 className="education-subtitle">사업영역</h1>
+            <img src={eduChartImage} alt="" />
+          </section>
+
+          <section className="education-description-forth">
+            <h1 className="education-subtitle">주요고객사</h1>
+            <p className="edu-forth-description">
+              수시스템의 기술교육재료는{" "}
+              <span>전국 고등학교(약 200곳)와 기술학원(약 57곳)에 납품</span>
+              되고 있으며,{" "}
+              <span>
+                국내 기술교육재료시장 점유율 1위로 전국기능경기대회 시험재료로
+                사용
+              </span>
+              되고 있습니다.
+            </p>
+          </section>
         </div>
       </section>
       <Footer />
